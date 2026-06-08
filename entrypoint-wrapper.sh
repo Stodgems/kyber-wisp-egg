502418b2656b2e0ef8aeeff09fdcfc30d4d8f3e5
Stodgems <brightwell04@gmail.com>
2026-06-08 20:14:57 +0100

    Fixes

--- entrypoint-wrapper.sh
+++ entrypoint-wrapper.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # Entrypoint wrapper for Kyber WISP/Pterodactyl egg
 # WISP mounts server files at /home/container and runs as user 999:999.
-# We pre-create the .local structure there before Kyber's entrypoint runs.
 # /root/.local and /mnt/battlefront are symlinked to /home/container at build time.
 
 mkdir -p /home/container/.local/share/kyber/module
-mkdir -p /home/container/.local/share/maxima
+mkdir -p /home/container/.local/share/maxima/wine/prefix
 mkdir -p /home/container/battlefront
 
 # Hand off to the original Kyber entrypoint
